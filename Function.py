f39bbee2f2154d349b18e5c5e95764a6e714a229
unknown <kehindealiu58@gmail.com>
2024-06-13 23:08:17 -0700

    This is another code

---
+++ Function.py
@@ -0,0 +1,77 @@
+# # # #Loop
+# # # #Function
+# # # #Classes
+# # # print = '55'
+# # # print("hello")
+# # # dict_my={}
+# # # dict_my
+# # # Nigeria = {'west':["Osun","Ogun","Ekiti","Ondo","Sokoto"],
+# # # 'North':["Kano","Yola","Jalimgo","Maiduguri"],
+# # # 'East':["Enugu", "abia","Delta","Rivers"]}
+# # # Nigeria
+# # # #DRY
+# # print(1)
+# # print(2)
+# # print(3)
+# # print(4)
+# # print(5)
+# # print(6)
+# # for num in range(6):
+# #     print(num)
+# # name = "nike"   
+# # print(name.upper())
+# # num = 401
+# # print(type(num))
+# # print(type(name))
+# # for num in range(1,10):
+# #     print(num)
+# # for num in range(1,12,2):
+# #     print(num)
+# # for num in range(5,12):
+# #     print(num)
+# # for num in range(5,13,4):
+# #     print(num)
+# # for num in range(4,15,3):
+# #     print(num)
+# # for num in range(13,2,-2):
+# #     print(num)
+# for num in range(3,37,3):
+#     print(num)
+# # Another code
+# for num in range(4):
+#     print(num)
+#     print("Dami is")
+    
+# print("please repeat me")
+# # multiplication table 3
+# for num in range(1,13):
+#     print(num*3)
+# names_list = ['damiloa', 'ade', 'ola', 'ayo']
+# for name in names_list:
+#     print(name)
+# list_length = len(names_list)
+# print(list_length)
+
+# for i in range(list_length):
+#     print(i)
+#     print(names_list[i])
+# for i in range(list_length):
+    
+# # print(name_list[i])
+# if i == 2:
+#     names_list[i] = 'Hajara'
+#     print(names_list[i])
+    
+# for num in range(1,7):
+#     print(num)
+# for letter in range(len('Damilola')):
+#     print(letter)
+    
+# name = "DamilolaAyobamidele"  
+# for i in range(len(name)):
+#     print(name[11])
+# print(len(name))
+
+LG_kwara = ["Ilorin west",'ekiti','oke-oro','ifelodun','barutee ','asa']
+for i in LG_kwara:
+    print(i[-1])
