0b0d7e7f9ba243b86c38fc1e174b144522256a46
unknown <kehindealiu58@gmail.com>
2024-06-13 23:37:44 -0700

    This is another code

--- Loop_Assignment.py
+++ Loop_Assignment.py
@@ -1,78 +1,78 @@
 # # For Loop ia used for iterating over a sequence (That is either a list, a tuple, a dictionary, a set, or a srting).
 # # Example
-# fruits = ['apple','banana','cherry']
-# for x in fruits:
-#     print(x)
+fruits = ['apple','banana','cherry']
+for x in fruits:
+     print(x)
 # # Exercise
-# for x in 'banana':
-#     print(x)
-#     # The "BREAK" Statement
-# fruits = ['apple','banana','cherry']
-# for x in fruits:
-#     print(x)
-#     if x == 'banana':
-#         break
-#     print(x)
-#     #Ex
-# fruits = ['apple','banana','cherry']
-# for x in fruits:
-#     if x == 'banana':
-#         break
-#     print(x)
-#     # The "CONTINUE" Statement
-#     fruits=['apple','banana','cherry']
-#     for x in fruits:
-#         if x == 'banana':
-#             continue
-#         print(x)
-# # The "RANGE()" Function
-# for x in range(6):
-#     print(x)
-# for x in range(2,6):
-#     print(x)
-# for x in range(2,30,3):
-#     print(x)
-# # the Else in for Loop
-# for x in range(6):
-#     print(x)
-# else:
-#     print("finally finished !")
-# for x in range(6):
-#     if x == 3: break
-#     print(x)
-# else:
-#     print("Finally finished !")
-# for x in range(10):
-#     if x != 7: break
-#     print(x)
-# else:
-#     print("Finally finished !")
-# # The NESTED Loop
-# adj = ["red","big", "tasty",]
-# fruits = ["apple","banana","cherry"]
-# for x in adj:
-#     for y in fruits:
-#      print(x ,y)
-# # The PASS Statement
-# for x in [0, 1,2]:
-#     pass
-# # The "WHILE" Loop
-# i = 1
-# while i < 6:
-#     print(i)
-#     i += 1
-#     i = 1
-# while i < 6:
-#     print(i)
+for x in 'banana':
+     print(x)
+     # The "BREAK" Statement
+fruits = ['apple','banana','cherry']
+for x in fruits:
+     print(x)
+     if x == 'banana':
+         break
+     print(x)
+     #Ex
+fruits = ['apple','banana','cherry']
+for x in fruits:
+    if x == 'banana':
+         break
+     print(x)
+     # The "CONTINUE" Statement
+fruits=['apple','banana','cherry']
+for x in fruits:
+         if x == 'banana':
+             continue
+         print(x)
+ # The "RANGE()" Function
+for x in range(6):
+     print(x)
+for x in range(2,6):
+     print(x)
+for x in range(2,30,3):
+     print(x)
+ # the Else in for Loop
+for x in range(6):
+     print(x)
+else:
+     print("finally finished !")
+for x in range(6):
+     if x == 3: break
+     print(x)
+else:
+     print("Finally finished !")
+ for x in range(10):
+     if x != 7: break
+     print(x)
+else:
+     print("Finally finished !")
+ # The NESTED Loop
+adj = ["red","big", "tasty",]
+fruits = ["apple","banana","cherry"]
+for x in adj:
+ for y in fruits:
+      print(x ,y)
+ # The PASS Statement
+for x in [0, 1,2]:
+     pass
+ # The "WHILE" Loop
+i = 1
+while i < 6:
+     print(i)
+     i += 1
+     i = 1
+while i < 6:
+     print(i)
 # Example
-# i = 1
-# while i < 6:
-#     print(i)
-#     if (i == 3):
-#         break 
-#     i+=1
-# i = 2
-# print(i)
+ i = 1
+while i < 6:
+     print(i)
+     if (i == 3):
+         break 
+     i+=1
+i = 2
+print(i)
 for i in [1,2,3]:
     pass
 i = 0
